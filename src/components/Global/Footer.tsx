76ae4881a48a322543a3ab33137cde0ff8bbd309
Aniebietabasi Obo <aniebietabasiobo@gmail.com>
2024-08-15 00:40:16 +0100

    commit

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -1,17 +1,68 @@
-import React from 'react'
-
+import React from "react";
+import Button from "./Button";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaRegCopyright, FaSquareXTwitter } from "react-icons/fa6";
+import { CiMail } from "react-icons/ci";
+import { LuPhone } from "react-icons/lu";
 function Footer() {
   return (
-    <div className='grid gap-y-[20px] p-[20px_30px]'>
-      <div className='grid grid-cols-4 '>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, repellendus ipsa. Corrupti, consequatur quisquam repellendus possimus asperiores, quo inventore ea, corporis quibusdam quis doloribus. Iste atque aut iusto soluta minus.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, repellendus ipsa. Corrupti, consequatur quisquam repellendus possimus asperiores, quo inventore ea, corporis quibusdam quis doloribus. Iste atque aut iusto soluta minus.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, repellendus ipsa. Corrupti, consequatur quisquam repellendus possimus asperiores, quo inventore ea, corporis quibusdam quis doloribus. Iste atque aut iusto soluta minus.</div>
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, repellendus ipsa. Corrupti, consequatur quisquam repellendus possimus asperiores, quo inventore ea, corporis quibusdam quis doloribus. Iste atque aut iusto soluta minus.</div>
+    <div className="grid gap-y-[20px] p-[20px_30px] font-manrope mt-[50px]">
+      <div className="grid  lg:grid-cols-4 gap-[40px]">
+        <div className=" ">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti,
+          repellendus ipsa. Corrupti, consequatur quisquam repellendus possimus
+          asperiores, quo inventore ea, corporis quibusdam quis doloribus. Iste
+          atque aut iusto soluta minus.
+        </div>
+        <div>
+          <p className="uppercase text-[20px] font-[600]">Contact us</p>
+          <div className="mt-[10px] flex flex-col space-y-[10px]">
+            <div className="flex items-center space-x-[10px]">
+              <CiMail  className="text-[#1A1A1A] text-[20px]"/>
+              <p className="text-[18px]">buildwithlove gmail.com</p>
+            </div>
+            <div className="flex items-center space-x-[10px]">
+            <LuPhone className="text-[#1A1A1A] text-[20px]" />
+              <p className="text-[18px]">+234 818 588 0532</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="uppercase text-[20px] font-[600]">Connect</p>
+          <div className="mt-[10px] flex flex-col space-y-[10px]">
+            <div className="flex items-center space-x-[10px]">
+              <FaLinkedinIn  className="text-[#1A1A1A] text-[20px]"/>
+              <p className="text-[18px]">LinkedIn</p>
+            </div>
+            <div className="flex items-center space-x-[10px]">
+            <FaSquareXTwitter className="text-[#1A1A1A] text-[20px]" />
+              <p className="text-[18px]">Twitter(X)</p>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <p className="uppercase text-[20px] font-[600]">
+            SUBSCRIBE to our Newsletter
+          </p>
+          <p>
+            Want to stay up to date with news and updates about our product?
+            Subscribe.
+          </p>
+          <div className="flex flex-row lg:flex-col w-full mt-[20px]  lg:space-y-[20px] space-x-[10px] lg:space-x-0">
+            <input
+              type="text"
+              className="w-full h-[40px] lg:h-[50px] px-[10px] rounded-[5px] border outline-none"
+            />
+            <Button type="filled" fill="#4B0082" text="Subscribe" />
+          </div>
+        </div>
+      </div>
+      <div className="w-full border-t p-[20px] border-[#CFCFD1] flex items-center justify-center space-x-[5px] mt-[50px]">
+      <FaRegCopyright />
+        <p className="text-[12px] lg:text-[16px]">Copyright 2024 .<span className="font-[500]">Buildwithlove</span>. All Right Reserved.</p>
+      </div>
     </div>
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque officia a minima perferendis harum iusto provident aliquid cupiditate distinctio cum ea dolorum sequi, architecto dignissimos inventore aspernatur pariatur repellendus dicta!</div>
-    </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Home/HomeHero.tsx
+++ src/components/Home/HomeHero.tsx
@@ -3,12 +3,14 @@ import Button from '../Global/Button'
 
 function HomeHero() {
   return (
-    <div className='space-y-[10px] lg:space-y-[20px] font-manrope py-[50px] flex flex-col items-center justify-center'>
+    <div className='space-y-[20px] lg:space-y-[40px] font-manrope py-[100px] flex flex-col items-center justify-center'>
         <p className='leading-[30px] lg:leading-[75px] font-[800] text-[24px] lg:text-[70px] text-center text-typography2'>Elevate Your <span className=' text-primary'>Business</span> with <br className='hidden lg:block' /> Stunning Digital Solutions</p>
         <p className='text-center text-[12px] lg:text-[16px] px-[10px]'>Welcome to Build With Love – where we create beautifully crafted websites and full  web applications for <br className='hidden lg:block' /> businesses of all sizes. Empower your online presence with our  expert services.</p>
         <div className="w-[160px] lg:w-[220px]">
           <Button type="filled" text="START PROJECT" fill="#4B0082" />
         </div>
+
+        <div className='h-[50px] w-full bg-[#4B0082] mt-[50px]'></div>
     </div>
   )
 }
